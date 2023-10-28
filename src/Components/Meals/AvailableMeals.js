47f869be8eae4e0e6d55b3106f47563141de8833
ShivpujanKr-NITJsr <sahil.8228053857@gmail.com>
2023-10-28 22:38:14 +0530

    added many components and css file for style ,also added card component and Input for ui

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,5 +1,8 @@
-
+import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css'
+
+import MealItem from './MealItem/MealItem';
+
 
 const DUMMY_MEALS = [
     {
@@ -32,13 +35,14 @@ const DUMMY_MEALS = [
 
 const AvailableMeals=()=>{
 
-    const mealsList =DUMMY_MEALS.map(meal=><li>{meal.name}</li>)
+    const mealsList =DUMMY_MEALS.map(meal=><MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>)
 
 
     return <section className={classes.meals}>
-        <ul>
-            {mealsList}
-        </ul>
+        <Card >
+
+            <ul>{mealsList}</ul>
+        </Card>
     </section>
 }
 
